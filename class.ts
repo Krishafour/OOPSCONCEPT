d7f749356ff71b9cf5dfc3b995858f811c67c269
Krishna <krishna.poul@afourtech.com>
2022-01-28 12:53:44 +0530

    class file Changed

--- class.ts
+++ class.ts
@@ -1,12 +1,16 @@
 class employee{
     id: number;
     name:string;
+    constructor(id: number,
+        name:string){
+       this.id=id;
+       this.name=name;
+    }
     greetings(){
         console.log('Hello World');
     }
 }
 
-const john=new employee();
-john.id=101;
-john.name="John";
+const john=new employee(101,"john");
+
 john.greetings();
