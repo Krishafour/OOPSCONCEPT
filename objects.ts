8b809d35f86aab2b60793deaeefa9537faa7e56c
Krishna <krishna.poul@afourtech.com>
2022-01-28 13:23:02 +0530

    objects file Changed

--- objects.ts
+++ objects.ts
@@ -2,6 +2,12 @@ class employee1{
     id:number
     name: string;
     isIncompany: boolean;
+    constructor()
+    {
+        this.id=0;
+        this.name='null';
+        this.isIncompany=true;
+    }
     greetings(){
         console.log(`Hello, my name is ${this.name},Id is ${this.id} Work in company is ${this.isIncompany}`);
     }
